3ca5b58f430548975e53ad3200c72f3a76be7171
NimmalaD <114887817+NimmalaD@users.noreply.github.com>
2022-12-02 19:47:24 -0500

    Modifying Ambulance Service UI

--- FinalprojectAED-main/src/MainUserInterface/Ambulance/AmbulanceInformationJPanel.java
+++ FinalprojectAED-main/src/MainUserInterface/Ambulance/AmbulanceInformationJPanel.java
@@ -16,7 +16,7 @@ import javax.swing.JPanel;
 
 /**
  *
- * @author Siddh
+ * @author indian
  */
 public class AmbulanceInformationJPanel extends javax.swing.JPanel {
 
@@ -164,7 +164,7 @@ public class AmbulanceInformationJPanel extends javax.swing.JPanel {
         jSeparator2.setBackground(new java.awt.Color(0, 0, 0));
         add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 90, 1434, -1));
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/MainUserInterface/Images/ambulance_blue.png"))); // NOI18N
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/MainUserInterface/Images/Ambulance_gif.gif"))); // NOI18N
         add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, -10, 1350, 1010));
     }// </editor-fold>//GEN-END:initComponents
 
